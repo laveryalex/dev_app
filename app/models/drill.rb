df0372a94452eaf0f75206f28b7e176a1b959c12
laveryalex <ubuntu@ip-172-31-14-193.us-east-2.compute.internal>
2020-09-17 02:35:25 +0000

    hw7

--- app/models/drill.rb
+++ app/models/drill.rb
@@ -1,10 +1,5 @@
 class Drill < ApplicationRecord
      has_and_belongs_to_many :player
-     attr_accessor :performance
-     attr_accessor :name
-     attr_accessor :sets
-     attr_accessor :reps
-     attr_accessor :description
      
     def print_drill
         return "#{name} sets: #{sets} reps: #{reps}  desc:#{description}"
